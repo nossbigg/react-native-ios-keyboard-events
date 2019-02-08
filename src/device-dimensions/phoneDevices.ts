3a6d632b72c8af23a2f493368e45bbaeb48d1066
nossbigg <chenggibson@gmail.com>
2019-02-08 10:47:04 +0800

    Add support for more iPhone devices
    iPhone 5/6/7/8/Plus/X/XS/XS Max/XR

--- src/device-dimensions/phoneDevices.ts
+++ src/device-dimensions/phoneDevices.ts
@@ -1,6 +1,35 @@
 import { IDeviceInformation } from "./deviceDimensions";
 
 const phoneDevices: IDeviceInformation[] = [
+  // For iPhone 5s, and iPhone SE
+  {
+    model: "iPhone 5s",
+    isTablet: false,
+    screenDimensions: new Set([320, 568]),
+    keyboardDimensions: {
+      landscape: {
+        docked: 199,
+      },
+      portrait: {
+        docked: 253,
+      },
+    },
+  },
+  // For iPhone 6/7/8
+  {
+    model: "iPhone 8",
+    isTablet: false,
+    screenDimensions: new Set([375, 667]),
+    keyboardDimensions: {
+      landscape: {
+        docked: 200,
+      },
+      portrait: {
+        docked: 260,
+      },
+    },
+  },
+  // For iPhone 6/7/8 Plus
   {
     model: "iPhone 8 Plus",
     isTablet: false,
@@ -11,6 +40,34 @@ const phoneDevices: IDeviceInformation[] = [
       },
       portrait: {
         docked: 271,
+      },
+    },
+  },
+  // For iPhone X/XS
+  {
+    model: "iPhone X",
+    isTablet: false,
+    screenDimensions: new Set([375, 812]),
+    keyboardDimensions: {
+      landscape: {
+        docked: 209,
+      },
+      portrait: {
+        docked: 335,
+      },
+    },
+  },
+  // For iPhone XS Max, and iPhone XR
+  {
+    model: "iPhone XS Max",
+    isTablet: false,
+    screenDimensions: new Set([414, 896]),
+    keyboardDimensions: {
+      landscape: {
+        docked: 209,
+      },
+      portrait: {
+        docked: 346,
       },
     },
   },
